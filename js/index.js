74255ecbd15cccdf5b5b4fbdcf249351507eb391
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2024-11-27 08:39:59 +0530

    Added json Data of three tables

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const bigCatsJson = [
+    { "species": "Big Cats", "name": "Tiger", "size": "10 ft", "location": "Asia" },
+    { "species": "Big Cats", "name": "Lion", "size": "8 ft", "location": "Africa" },
+    { "species": "Big Cats", "name": "Leopard", "size": "5 ft", "location": "Africa and Asia" },
+    { "species": "Big Cats", "name": "Cheetah", "size": "5 ft", "location": "Africa" },
+    { "species": "Big Cats", "name": "Caracal", "size": "3 ft", "location": "Africa" },
+    { "species": "Big Cats", "name": "Jaguar", "size": "5 ft", "location": "Amazon" }
+];
+
+const dogsJson = [
+    { "species": "Dog", "name": "Rottweiler", "size": "2 ft", "location": "Germany" },
+    { "species": "Dog", "name": "German Shepherd", "size": "2 ft", "location": "Germany" },
+    { "species": "Dog", "name": "Labrador", "size": "2 ft", "location": "UK" },
+    { "species": "Dog", "name": "Alabai", "size": "4 ft", "location": "Turkey" }
+];
+
+const bigFishJson = [
+    { "species": "Big Fish", "name": "Humpback Whale", "size": "15 ft", "location": "Atlantic Ocean" },
+    { "species": "Big Fish", "name": "Killer Whale", "size": "12 ft", "location": "Atlantic Ocean" },
+    { "species": "Big Fish", "name": "Tiger Shark", "size": "8 ft", "location": "Ocean" },
+    { "species": "Big Fish", "name": "Hammerhead Shark", "size": "8 ft", "location": "Ocean" }
+];
+
